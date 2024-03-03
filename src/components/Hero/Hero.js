1b5ecf26176ba0be1d261ee249540ecc195d9a8e
sameer sheikh <sameersheikh819@gmail.com>
2024-03-03 14:23:40 +0530

    add hero section

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,17 @@
+import React from "react";
+import headphone from "../../assets/headphone.png";
+import styles from "./Hero.module.css";
+
+const Hero = () => {
+  return (
+    <section className={styles.heroWrapper}>
+      <div>
+        <h1>100 Thousand Songs, ad-free Over</h1>
+        <h1> thousands podcast episodes</h1>
+      </div>
+      <img src={headphone} alt="" />
+    </section>
+  );
+};
+
+export default Hero;
