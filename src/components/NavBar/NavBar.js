200efa9e1fcc9172b832ed3d2f3534509ee828a1
sameer sheikh <sameersheikh819@gmail.com>
2024-03-03 13:50:38 +0530

    .

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,37 +1,39 @@
-// import { Button } from "./Button/Button";
-// import Logo from "../../assets/logo.png";
-// import * as styles from "./NavBar.module.css";
-// import Search from "./Search/Search";
-// const NavBar = () => {
-//   return (
-//     <nav className={styles.navBar}>
-//       <div className={styles.logoDiv}>
-//         <img src={Logo} alt="logo" width={67} />
-//       </div>
-//       <Search placeholder="Search a album of your choice" />
-//       <Button children="GIVE FEEDBACK" />
-//     </nav>
-//   );
-// };
-
-// export default NavBar;
-
-import React from "react";
 import Button from "./Button/Button";
+import Logo from "../../assets/logo.png";
+import * as styles from "./NavBar.module.css";
 import Search from "./Search/Search";
-import styles from "./NavBar.module.css";
-import LogoImage from "../../assets/Logo.png";
-
-export default function Navbar() {
+const NavBar = () => {
   return (
     <>
-      <nav className={styles.navbar}>
+      <nav className={styles.navBar}>
         <div className={styles.logoDiv}>
-          <img src={LogoImage} alt="logo" width={67} />
+          <img src={Logo} alt="logo" width={67} />
         </div>
-        <Search search={"Search a song of your choice"} />
-        <Button children="Give Feedback" />
+        <Search placeholder="Search a album of your choice" />
+        <Button children="GIVE FEEDBACK" />
       </nav>
     </>
   );
-}
+};
+
+export default NavBar;
+
+// import React from "react";
+// import Button from "./Button/Button";
+// import Search from "./Search/Search";
+// import styles from "./NavBar.module.css";
+// import LogoImage from "../../assets/logo.png";
+
+// export default function Navbar() {
+//   return (
+//     <>
+//       <nav className={styles.navbar}>
+//         <div className={styles.logoDiv}>
+//           <img src={LogoImage} alt="logo" width={67} />
+//         </div>
+//         <Search search={"Search a song of your choice"} />
+//         <Button children="Give Feedback" />
+//       </nav>
+//     </>
+//   );
+// }
