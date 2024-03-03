24dc0dac90e50b9c9842afac74939e876ec9b879
sameer sheikh <sameersheikh819@gmail.com>
2024-03-03 13:35:27 +0530

    modify code

--- src/components/NavBar/Button/Button.js
+++ src/components/NavBar/Button/Button.js
@@ -1,5 +1,14 @@
-import * as styles from "./Button.module.css";
+// import * as styles from "./Button.module.css";
 
-export const Button = ({ children }) => {
-  return <div className={styles.btn}>{children}</div>;
+// export const Button = ({ children }) => {
+//   return <div className={styles.btn}>{children}</div>;
+// };
+
+import React from "react";
+import styles from "./Button.module.css";
+
+const Button = ({ children }) => {
+  return <button className={styles.button}>{children}</button>;
 };
+
+export default Button;
--- src/components/NavBar/Search/Search.js
+++ src/components/NavBar/Search/Search.js
@@ -1,19 +1,36 @@
-import React from "react";
-import * as styles from "./Search.module.css";
-import SearchIcon from "jsx:../../../assets/searchIcon.svg";
+// import React from "react";
+// import * as styles from "./Search.module.css";
+// import SearchIcon from "jsx:../../../assets/searchIcon.svg";
 
-const Search = ({ placeholder }) => {
+// const Search = ({ placeholder }) => {
+//   return (
+//     <div>
+//       <form className={styles.wrapper}>
+//         <input placeholder={placeholder} className={styles.search} />
+//         <div>
+//           <button className={styles.searchButton} type="submit">
+//             <SearchIcon />
+//           </button>
+//         </div>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default Search;
+
+import React from "react";
+import SearchIcon from "jsx:../../../assets/searchIcon.svg";
+import styles from "./Search.module.css";
+
+const Search = ({ search }) => {
   return (
-    <div>
-      <form className={styles.wrapper}>
-        <input placeholder={placeholder} className={styles.search} />
-        <div>
-          <button className={styles.searchButton} type="submit">
-            <SearchIcon />
-          </button>
-        </div>
-      </form>
-    </div>
+    <form className={styles.wrapper}>
+      <input className={styles.search} placeholder={search}></input>
+      <button className={styles.searchButton} type="submit">
+        <SearchIcon />
+      </button>
+    </form>
   );
 };
 
