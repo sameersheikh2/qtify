faa2b14bb618bb8f910e116c43ec6b6fc5346e4a
sameer sheikh <sameersheikh819@gmail.com>
2024-03-07 09:38:06 +0530

    grid of cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
 import Hero from "./components/Hero/Hero";
 import NavBar from "./components/NavBar/NavBar";
+import Section from "./components/Section/Section";
+import { fetchNewAlbums, fetchTopAlbums } from "./api/api";
+import { useEffect, useState } from "react";
 
 const App = () => {
+  const [topAlbumSongs, setTopAlbumSongs] = useState([]);
+  const [newAlbumSongs, setNewAlbumSongs] = useState([]);
+
+  const generateTopAlbumSongs = async () => {
+    try {
+      const topAlbumSongs = await fetchTopAlbums();
+      setTopAlbumSongs(topAlbumSongs);
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  };
+
+  const generateNewAlbumSongs = async () => {
+    try {
+      const newAlbumSongs = await fetchNewAlbums();
+      setNewAlbumSongs(newAlbumSongs);
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  };
+
+  useEffect(() => {
+    generateTopAlbumSongs();
+    generateNewAlbumSongs();
+  }, []);
+
   return (
     <>
       <NavBar />
       <Hero />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "32px",
+          padding: "0px 30px 60px",
+        }}
+      >
+        <Section type="album" title="Top Albums" data={topAlbumSongs} />
+        <Section type="album" title="New Albums" data={newAlbumSongs} />
+      </div>
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import App from "./App";
 import "../index.css";
+import "swiper/swiper-bundle.css";
 import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.getElementById("root"));
